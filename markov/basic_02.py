5b30de9c759c971b15e315ddb6cb9be204669e45
Hapiel <daniel@danielsiegmund.nl>
2021-10-26 12:37:55 +0200

    using pathlib

--- markov/basic_02.py
+++ markov/basic_02.py
@@ -1,12 +1,15 @@
 import markovify
 import os
+from pathlib import Path
+
+data_folder = Path("text/tp_texts_small/")
 
 combined_model = None
-for (dirpath, _, filenames) in os.walk("text/tp_texts_small"):
+for (dirpath, _, filenames) in os.walk(data_folder):
     for filename in filenames:
         with open(os.path.join(dirpath, filename)) as f:
             # print (dirpath + " " + filename)
-            model = markovify.Text(f, retain_original=False, state_size=3)
+            model = markovify.Text(f, retain_original=False, state_size=4)
             if combined_model:
                 combined_model = markovify.combine(models=[combined_model, model])
             else:
@@ -14,5 +17,5 @@ for (dirpath, _, filenames) in os.walk("text/tp_texts_small"):
 
 
 for i in range(5):
-    print(combined_model.make_sentence())
+    print(combined_model.make_sentence(test_output = False))
 
