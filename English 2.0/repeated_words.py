96a38800fe30c64c3044bd3e01e550e77ffe1d70
Hanastasiah <anastasia.thambwe.ser@gmail.com>
2021-10-30 09:37:02 +0200

    Array of words
    Creation of an array of words for searching for repeated words

--- English 2.0/repeated_words.py
+++ English 2.0/repeated_words.py
@@ -4,6 +4,9 @@ import csv
 
 #to check what line of the csv file / what text is worked on
 iter_count = 0
+construction_word = ''
+current_word = ''
+words = []
 
 with open('../english/kaggle_poem_dataset.csv', newline='') as csvfile:
     textreader = csv.reader(csvfile, delimiter=',')
@@ -14,10 +17,18 @@ with open('../english/kaggle_poem_dataset.csv', newline='') as csvfile:
         #print(row[4])
 
         for i in row[4]:
+            if i != ' ' and i != '\n' and i != ',' and i != ':' and i != '.' and i != '-' and i != '"' and i != '?' and i != '!':
+                current_word = construction_word + i
+                construction_word = current_word
             if i == ' ' or i == '\n':
                 word_count += 1
+                words.append(current_word)
+                construction_word = ''
 
         #to have to correct amount of words
         word_count += 1
-        print("Text " + str(iter_count))
-        print(word_count)
+        #print("Text " + str(iter_count))
+        #print(word_count)
+        print(words)
+
+
