e7e33df2cc621aea9d22da64c823101fb130bd0b
Semma Raadschelders <semma2@live.nl>
2021-11-02 15:14:12 +0100

    Split linelength and syllable count per line.

--- english/analyse.py
+++ english/analyse.py
@@ -26,34 +26,30 @@ def analysePoem(poem):
     noLines = len(lines)
 
     noEmptyLines = 0
-    wordSum = 0
-    syllableSum = 0
+    syllableCounts = []
+    lineLenghts = []
     for line in lines:
         if(line == ""):
             noEmptyLines += 1
-        wordSum += len(line.split())
-        syllableSum += syllableCount(line)
+        lineLenghts.append(len(line.split()))
+        syllableCounts.append(syllableCount(line))
 
-    averageLineLength = wordSum / noLines
-    averageSyllables = syllableSum / noLines
+    return noLines, noEmptyLines, lineLenghts, syllableCounts
 
-    return noLines, noEmptyLines, averageLineLength, averageSyllables
+with open('kaggle_poem_dataset.csv', newline='', encoding="utf-8") as f:
+    reader = csv.reader(f)
+    for i in reader:
+        if(i[0] == 'x'):
+            continue
+        # Useful in case you want to check if it works quickly.
+        elif(i[0] == '10'):
+            break
+        noLines, noEmptyLines, lineLenghts, syllableCounts = analysePoem(i[CONTENT])
 
-# with open('kaggle_poem_dataset.csv', newline='', encoding="utf-8") as f:
-#     reader = csv.reader(f)
-#     for i in reader:
-#         if(i[0] == 'x'):
-#             continue
-#         # Useful in case you want to check if it works quickly.
-#         elif(i[0] == '20'):
-#             break
-#         noLines, noEmptyLines, averageLineLength, averageSyllables = analysePoem(i[CONTENT])
+        print(i[TITLE])
+        print("noLines: ", noLines)
+        print("noEmptylines: ", noEmptyLines)
+        print("averageLineLength: ", lineLenghts)
+        print("averageSyllables: ", syllableCounts)
+        print()
 
-#         print(i[TITLE])
-#         print("noLines: ", noLines)
-#         print("noEmptylines: ", noEmptyLines)
-#         print("averageLineLength: ", averageLineLength)
-#         print("averageSyllables: ", averageSyllables)
-#         print()
-
-print(syllableCount("o toki mi wile ala moku e kili lili..."))
