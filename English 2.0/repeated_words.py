12fb8bd291a5d97df31132de209a47d63b576acc
Hanastasiah <anastasia.thambwe.ser@gmail.com>
2021-11-06 13:40:04 +0100

    Comments

--- English 2.0/repeated_words.py
+++ English 2.0/repeated_words.py
@@ -4,14 +4,12 @@ import random
 
 #to check what line of the csv file / what text is worked on
 text_index = 0 #a text index value used when the code goes over every single poem of the database, one by one (the value is increment with each peom that is covered
-construction_word = ''
-current_word = ''
-random_text_index = random.randint(1, 15651)
-inputPoem = ''
-word_count = 0
-words = []
-
-
+construction_word = '' #used to construct a word during the loop going through each sentence of the poem with current_word
+current_word = '' #used to construct a word during the loop going through each sentence of the poem with construction_word
+random_text_index = random.randint(1, 15651) #choses a random entry in the english poem database
+inputPoem = '' #string variable used to store the poem entered by the user
+word_count = 0 #integer variable to keep track of the amount of words in the text
+words = [] #list to store all words of the poem without the spaces, punctuation, etc
 
 control_value = ''
 
@@ -55,19 +53,20 @@ def repeat_word_count(repeated_words, words):
     # reset repeated_words
     repeated_words = []
 
+#Beginning of the console printing
 print("Welcome to PoetGenerator 101.")
-# themeYesNo = input("Would you like to select a theme? (Enter 'Y' for YES or 'N' for NO) : ")
-# print("You entered : " + themeYesNo)
-# if (themeYesNo == "y" or themeYesNo == "Y"):
-#     theme = input("Please enter a word for the theme of your poem : ")
-#     print("You entered : " + theme)
 
+#Questions about the theme and taking the user's input
+themeYesNo = input("Would you like to select a theme? (Enter 'Y' for YES or 'N' for NO) : ")
+print("You entered : " + themeYesNo)
+if (themeYesNo == "y" or themeYesNo == "Y"):
+    theme = input("Please enter a word for the theme of your poem : ")
+    print("You entered : " + theme)
 
+#Questions about the input poem and taking in the user's inputs
 inputPoemYesNo = input(
     "Would you like to enter a poem as a reference for the generated poem? You will need to specify how many characters this poem has (Enter 'Y' for YES or 'N' for NO) : ")
 print("You entered : " + inputPoemYesNo)
-
-
 if (inputPoemYesNo == "y" or inputPoemYesNo == "Y"):
     characters_input_poem = input("Please enter the amount of characters your poem has : ")
     print("Please, copy/paste the poem you want to use as a reference and press the key 'ENTER' : ")
@@ -85,11 +84,9 @@ if (inputPoemYesNo == "y" or inputPoemYesNo == "Y"):
     words = words_list_maker(inputPoem)
     print(words)
 
-    #repeat_word_count()
 
 else:
     # English analysis functions for the database random poem
-
     with open('../english/kaggle_poem_dataset.csv', newline='', encoding="utf-8") as csvfile:
         textreader = csv.reader(csvfile, delimiter=',')
 
@@ -125,9 +122,6 @@ else:
 
                 # Call the repeated word function
                 repeat_word_count(repeated_words, words)
-
-
-
 
 
             #to have to correct amount of words
